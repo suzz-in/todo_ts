2d42fafe03c381dedb82def0b5c28927a6ccfd84
suzz-in <o_o_su_jin_@naver.com>
2022-12-07 15:36:05 +0900

    feat : list warp 설정

--- todo/src/components/TodoList.tsx
+++ todo/src/components/TodoList.tsx
@@ -1,11 +1,17 @@
+import styled from "styled-components";
 import { useTodosState } from "../context/TodosContext";
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
     const todos = useTodosState();
-    return (<>
+    return (<ListWarp>
         {todos.length === 0 ? <span>오늘의 할 일을 입력해주세요😝</span> : todos.map((todo)=> <TodoItem todo={todo} key={todo.id}/> )}
-    </>)
+    </ListWarp>)
 }
 
 export default TodoList;
+
+const ListWarp = styled.div`
+margin: 4vh auto auto auto ;
+width : 70vw;
+`
